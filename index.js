f17faa216b2c7385080bdcd2a56e434d46ab40d6
Sayhan Ahmed <sayanahmed228@gmail.com>
2025-12-16 17:04:58 +0600

    Add: JWT token verify middleware

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// JWT
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+
+  // Verify the token
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded; 
+    next();
+  });
+};
+
 // MongoDB URI
 const uri = process.env.MONGODB_URI;
 if (!uri) {
